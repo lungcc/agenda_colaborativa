274bd4079762f2c424e4198e8680e0cf6164e00c
giulianovarriale <giuliano.oca@gmail.com>
2014-10-03 17:22:12 -0300

    api rodando, upload images no gitignore

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,26 +3,43 @@
 (function(){
 
 
-	$.ajax({
-		url: 'localhost:8080/api/eventos',
-		type: 'GET',
-		// contentType: 'application/json',
-		dataType: 'jasonp',
-		crossDomain: true
-	}).done(function(data){
-		console.log(data);
-	});
+	// $.ajax({
+	// 	url: 'localhost:8080/api/eventos',
+	// 	type: 'GET',
+	// 	// contentType: 'application/json',
+	// 	dataType: 'jasonp',
+	// 	crossDomain: true
+	// }).done(function(data){
+	// 	console.log(data);
+	// });
 
-	// var app = angular.module('store', []);
+	var app = angular.module('store', []);
 
-	// app.controller('EventosController', [ '$http', function($http){
-	// 	var eventos = this;
-	// 	eventos.item = [];
+	app.controller('EventosController', [ '$http', function($http){
+		var eventos = this;
+		eventos.item = [];
 
-	// 	$http.get('localhost:8080/api/eventos').success(function(data){
-	// 		console.log(data);
-	// 		eventos.item = data;
-	// 	});
-	// }]); 
+		$http({
+			url : 'http://localhost:8080/api/eventos',
+			method : "GET"    
+		}).then(function(response){
+			eventos.item = response.data;
+			console.log(eventos.item);
+		});
+
+	}]); 
+
+
+
+	// myApp.controller("MyCtrl",['$scope','$http','$window',function($scope,$http,$window){  
+	  
+	//   $http({
+	//     url : 'http://www.corsproxy.com/muslimsalat.com/dhaka/monthly.json',
+	//     method : "GET"    
+	//   }).then(function(response){
+	//     $scope.data=response.data;
+	//   });
+	
+
 
 })();
