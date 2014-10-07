bf3052314d53e64e19fa50b81828c1f97a78eaf5
guilherme lopes <guiajlopes@gmail.com>
2014-10-07 13:56:09 -0300

    Removendo linhas comentadas no app.js do front

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,45 +1,16 @@
 // console.log('opa');
 
 (function(){
+  var app = angular.module('store', []);
+  app.controller('EventosController', [ '$http', function($http){
+    var eventos = this;
+    eventos.item = [];
 
-
-	// $.ajax({
-	// 	url: 'localhost:8080/api/eventos',
-	// 	type: 'GET',
-	// 	// contentType: 'application/json',
-	// 	dataType: 'jasonp',
-	// 	crossDomain: true
-	// }).done(function(data){
-	// 	console.log(data);
-	// });
-
-	var app = angular.module('store', []);
-
-	app.controller('EventosController', [ '$http', function($http){
-		var eventos = this;
-		eventos.item = [];
-
-		$http({
-			url : 'http://localhost:8080/api/eventos',
-			method : "GET"
-		}).then(function(response){
-			eventos.item = response.data;
-			console.log(eventos.item);
-		});
-
-	}]);
-
-
-
-	// myApp.controller("MyCtrl",['$scope','$http','$window',function($scope,$http,$window){
-
-	//   $http({
-	//     url : 'http://www.corsproxy.com/muslimsalat.com/dhaka/monthly.json',
-	//     method : "GET"
-	//   }).then(function(response){
-	//     $scope.data=response.data;
-	//   });
-
-
-
+    $http({
+      url : 'http://localhost:8080/api/eventos',
+      method : "GET"
+    }).then(function(response){
+      eventos.item = response.data;
+    });
+  }]);
 })();
